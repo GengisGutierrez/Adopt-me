1f89ab7c24bda3ba2ccc66cdf20cd92b3184406f
Gengis <gengis.gutierrez.c@gmail.com>
2022-04-07 13:34:01 -0500

    Some changes

--- src/Pet.js
+++ src/Pet.js
@@ -1,4 +1,4 @@
-// import React from 'react';
+import { Link } from "react-router-dom";
 
 const Pet = ({
   name,
@@ -17,7 +17,7 @@ const Pet = ({
   }
 
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name}/>
       </div>
@@ -26,7 +26,7 @@ const Pet = ({
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
       
-    </a>
+    </Link>
   )
 }
 
