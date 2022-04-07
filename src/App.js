50a1b5a5720b61460776ce4a29315bff20aa8afd
Gengis <gengis.gutierrez.c@gmail.com>
2022-04-07 11:52:55 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import SearchParams from './SearchParams';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+      <SearchParams />
+    </div>
+  )
+}
+ReactDOM.render(
+  <StrictMode>
+    <App/>
+  </StrictMode>,
+  document.getElementById("root"));
